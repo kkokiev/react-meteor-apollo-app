d975883829a7f900efab57088a5b569efda3ec70
ConstantineKononenko <constantine.kononenko@gmail.com>
2018-01-20 09:42:11 +0200

    add init files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+    "jest": true
+  },
+  "rules": {
+    "react/require-default-props": [0],
+    "comma-dangle": [0],
+    "import/no-extraneous-dependencies": [0],
+    "import/no-unresolved": [0],
+    "react/jsx-filename-extension": [0],
+    "object-curly-newline": [0],
+    "function-paren-newline": [0],
+    "react/jsx-wrap-multilines": [0],
+    "import/extensions": [2, "never"],
+    "react/forbid-prop-types": [0],
+    "jsx-a11y/anchor-is-valid": [0],
+    "react/no-array-index-key": [0],
+    "no-shadow": [0],
+    "no-confusing-arrow": [0]
+  }
+};
